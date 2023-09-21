98e3ca248228e39a2b820cbf7db34cb2f22e3023
chhavijain <chhavijain221864@gmail.com>
2023-09-21 14:20:58 +0530

    files added

---
+++ app/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Product,Customer,Cart,Payment,OrderPlaced
+# Register your models here.
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id','title','discounted_price','category','product_image']
+    
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','locality','city','state','zipcode']    
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+   list_display=['id','user','product','quantity']    
+   
+@admin.register(Payment)
+class PaymentModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+
+@admin.register(OrderPlaced)
+class OrderPlacedAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','quantity','ordered_date','status','payment']    
---
+++ app/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+STATE_CHOICES={
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Goa','Goa'),
+    ('Delhi','Delhi'),
+    ('MP','MP'),
+}
+CATEGORY_CHOICES={
+    ('CR','Curd'),
+    ('ML','Milk'),
+    ('LS','Lassi'),
+    ('MS','Milkshake'),
+    ('PN','Paneer'),
+    ('GH','Ghee'),
+    ('CZ','Cheese'),
+    ('IC','Ice-cream'),
+}
+
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description=models.TextField()
+    composition=models.TextField(default='')
+    prodapp=models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    locality=models.CharField(max_length=200)
+    city=models.CharField(max_length=50)
+    mobile=models.IntegerField(default=0)
+    zipcode=models.IntegerField()
+    state=models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name    
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    
+    @property
+    def total_cost(self):
+       return self.quantity*self.product.discounted_price    
+
+STATUS_CHOICES=(
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel'),
+    ('Pending','Pending'),
+)
+
+class Payment(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE) 
+    amount=models.FloatField()
+    razorpay_order_id=models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_status=models.CharField(max_length=100,blank=True,null=True)
+    razorpay_payment_id=models.CharField(max_length=100,blank=True,null=True)
+    paid=models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE) 
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    ordered_date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+    payment=models.ForeignKey(Payment,on_delete=models.CASCADE,default="")
+    @property
+    def total_cost(self):
+        return self.quantity*self.product.discounted_price
